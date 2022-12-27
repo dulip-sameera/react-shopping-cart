6cf0ceba45e034477a73f3f23155a0249ef7e790
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-27 08:31:14 +0530

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "762px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        customBlack: "#0B0C10",
+        customDarkGrey: "#1F2833",
+        customLightGrey: "#C5C6C7",
+        customCyan: "#66FCF1",
+        customDarkCyan: "#45A29E",
+      },
+    },
   },
   plugins: [],
 };
