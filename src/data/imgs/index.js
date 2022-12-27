03ccf1128e2993efe4c6154e351217ba10077016
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-27 08:01:56 +0530

    add all store items

---
+++ src/data/imgs/index.js
@@ -0,0 +1,31 @@
+import img1 from "./1.jpg";
+import img1BG from "./1_BG.jpg";
+import img2 from "./2.jpg";
+import img2BG from "./2_BG.jpg";
+import img3 from "./3.jpg";
+import img3BG from "./3_BG.jpg";
+import img4 from "./4.jpg";
+import img4BG from "./4_BG.jpg";
+import img5 from "./5.jpg";
+import img5BG from "./5_BG.jpg";
+import img6 from "./5.jpg";
+import img6BG from "./5_BG.jpg";
+import BG from "./BG.jpg";
+import cartBG from "./cartBG.jpg";
+
+export {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img1BG,
+  img2BG,
+  img3BG,
+  img4BG,
+  img5BG,
+  img6BG,
+  BG,
+  cartBG,
+};
---
+++ src/data/store_itesm.js
@@ -0,0 +1,71 @@
+import {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img1BG,
+  img2BG,
+  img3BG,
+  img4BG,
+  img5BG,
+  img6BG,
+} from "./imgs";
+
+export const itemList = [
+  {
+    id: "1",
+    name: "Harley Davidson - Nightster-Cruiser",
+    description:
+      "Mauris ullamcorper nostrud nostra exercitation elementum? Labore praesent turpis sint optio doloremque? Tincidunt debitis, consequat, incididunt suscipit officia, anim deserunt, maecenas sit leo aute? Auctor.",
+    imgURL: img1,
+    imgBgURL: img1BG,
+    price: "12000",
+  },
+  {
+    id: "2",
+    name: "Honda-CBR1000rr FireBlade",
+    description:
+      "Mauris ullamcorper nostrud nostra exercitation elementum? Labore praesent turpis sint optio doloremque? Tincidunt debitis, consequat, incididunt suscipit officia, anim deserunt, maecenas sit leo aute? Auctor.",
+    imgURL: img2,
+    imgBgURL: img2BG,
+    price: "10000",
+  },
+  {
+    id: "3",
+    name: "Suzuki GSX800RQM3",
+    description:
+      "Mauris ullamcorper nostrud nostra exercitation elementum? Labore praesent turpis sint optio doloremque? Tincidunt debitis, consequat, incididunt suscipit officia, anim deserunt, maecenas sit leo aute? Auctor.",
+    imgURL: img3,
+    imgBgURL: img3BG,
+    price: "12000",
+  },
+  {
+    id: "4",
+    name: "Honda-CMX-1100",
+    description:
+      "Mauris ullamcorper nostrud nostra exercitation elementum? Labore praesent turpis sint optio doloremque? Tincidunt debitis, consequat, incididunt suscipit officia, anim deserunt, maecenas sit leo aute? Auctor.",
+    imgURL: img4,
+    imgBgURL: img4BG,
+    price: "16000",
+  },
+  {
+    id: "5",
+    name: "Honda-CRF450R",
+    description:
+      "Mauris ullamcorper nostrud nostra exercitation elementum? Labore praesent turpis sint optio doloremque? Tincidunt debitis, consequat, incididunt suscipit officia, anim deserunt, maecenas sit leo aute? Auctor.",
+    imgURL: img5,
+    imgBgURL: img5BG,
+    price: "11000",
+  },
+  {
+    id: "6",
+    name: "Honda-GoldWing-GL1800-Tour-2022",
+    description:
+      "Mauris ullamcorper nostrud nostra exercitation elementum? Labore praesent turpis sint optio doloremque? Tincidunt debitis, consequat, incididunt suscipit officia, anim deserunt, maecenas sit leo aute? Auctor.",
+    imgURL: img6,
+    imgBgURL: img6BG,
+    price: "15000",
+  },
+];
