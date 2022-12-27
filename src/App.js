fb7113bef9b0562129222a5f5ea38ee823c827c4
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-27 08:26:12 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path={"/"} element={<Layout />}>
+        <Route index element={"Home"} />
+        <Route path={"/store"} element={"Store"} />
+        <Route path={"/store/:itemId"} element={"StoreItem"} />
+        <Route path={"/about"} element={"About"} />
+        <Route path={"*"} element={"Error"} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
