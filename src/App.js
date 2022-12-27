fc8cec05c81a9c0c5d8b1706ff498721398aa7e9
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-27 10:55:06 +0530

    add home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 
 const App = () => {
   return (
     <Routes>
       <Route path={"/"} element={<Layout />}>
-        <Route index element={"Home"} />
+        <Route index element={<Home />} />
         <Route path={"/store"} element={"Store"} />
         <Route path={"/store/:itemId"} element={"StoreItem"} />
         <Route path={"/about"} element={"About"} />
