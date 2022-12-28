e5a73c040b81ff89079115b7bbed171e1e06a2a6
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-28 07:27:26 +0530

    add bgImg Reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { bgImgReducer } from "./features/bgImage.slice";
 import { cartReducer } from "./features/cart.slice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    bgImg: bgImgReducer,
   },
 });
