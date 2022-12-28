8b85fd8b849ff8795932b1145239617b54aae2d9
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-28 05:57:21 +0530

    add store route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
+import Store from "./pages/Store";
 
 const App = () => {
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+
   return (
     <Routes>
       <Route path={"/"} element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path={"/store"} element={"Store"} />
+        <Route path={"/store"} element={<Store />} />
         <Route path={"/store/:itemId"} element={"StoreItem"} />
         <Route path={"/about"} element={<About />} />
         <Route path={"/cart"} element={"cart"} />
